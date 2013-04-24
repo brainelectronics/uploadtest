4380dacf44ab6a4fdab8f0d65012d2967cf16e41
brainelectronics <info@brainelectronics.de>
2013-04-25 00:42:18 +0200

    bla

--- GitHubTest/ViewController.m
+++ GitHubTest/ViewController.m
@@ -19,6 +19,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     //sinnloese kommentar
+    //nochmal was
 }
 
 - (void)didReceiveMemoryWarning
